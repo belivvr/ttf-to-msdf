27002a4d51ac9039c777f99898dbfc374875bd46
juunini <juuni.ni.i@gmail.com>
2021-11-04 08:54:00 +0900

    index.ts 파일의 작동을 완성합니다.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
-export default function sum(a: number, b: number): number {
-  return a + b;
-}
+import fs from 'fs';
+// @ts-ignore
+import generateBMFont from 'msdf-bmfont-xml';
+
+import getOnlyTTF from './getOnlyTTF';
+import invertImage from './invertImage';
+import config from '../config.json';
+
+const charset: string = fs.readFileSync('charset.txt', { encoding: 'utf-8' });
+
+const allFilenames: string[] = fs.readdirSync('ttf');
+const ttfFilenames: string[] = getOnlyTTF(allFilenames);
+
+ttfFilenames.forEach((filename: string) => {
+  const ttfFile: string = `ttf/${filename}.ttf`;
+
+  generateBMFont(
+    ttfFile,
+    { ...config, charset },
+    async (err: string, textures: any[], font: any) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      if (!fs.existsSync('msdf')) {
+        fs.mkdirSync('msdf');
+      }
+      fs.writeFileSync(`msdf/${filename}.${config.outputType}`, font.data);
+
+      textures.forEach(async (texture, index) => {
+        const imageFileName: string = `msdf/${filename}${index > 0 ? index : ''}.png`;
+        const image: Buffer = await invertImage(texture.texture);
+        fs.writeFileSync(imageFileName, image);
+      });
+    },
+  );
+});
